7c71ccd0c0646485de3f9b600e1faef2457a229a
adrianxx321 <lianghan321@gmail.com>
2021-07-10 15:46:44 +0800

    Set product page as launch screen

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { NavigationContainer } from "@react-navigation/native"
-import Tabs from "./src/components/Tabs"
+import Product from './src/screens/Product';
+// createAppContainer has been changed to NavigationContainer
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tabs />
+        <Stack.Navigator
+          headerMode="none"
+        >
+          <Stack.Screen name="Product" component={Product} />
+        </Stack.Navigator>
     </NavigationContainer>
-  );
+  )
 }
