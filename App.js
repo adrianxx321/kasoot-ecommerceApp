44defda76de0dfd9d9a2fd91c2bdb61e91579d4a
adrianxx321 <lianghan321@gmail.com>
2021-08-09 23:37:24 +0800

    Add firebase

--- App.js
+++ App.js
@@ -1,18 +1,29 @@
 import React from 'react';
-import Product from './src/screens/Product';
 // createAppContainer has been changed to NavigationContainer
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import * as firebase from 'firebase'
+
+import Product from './src/screens/Product'
+import SearchProductsScreen from './src/screens/SearchProductsScreen';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: 'AIzaSyA3vh9VqlkPZfTEosvvRnTb8EW80A5aDGo',
+  authDomain: 'kasoot-1920c.firebaseapp.com',
+  projectId: 'kasoot-1920c',
+  databaseURL: 'https://kasoot-1920c.firebaseio.com',
+  storageBucket: 'kasoot-1920c.appspot.com',
+};
+!firebase.apps.length ? firebase.initializeApp(firebaseConfig).firestore(): firebase.app().firestore()
 
 const Stack = createStackNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
-        <Stack.Navigator
-          headerMode="none"
-        >
-          <Stack.Screen name="Product" component={Product} />
+        <Stack.Navigator headerMode="none">
+          <Stack.Screen name="Search Products" component={SearchProductsScreen} />
         </Stack.Navigator>
     </NavigationContainer>
   )
