ff08d33ec046be41f712d7a173fe88276f6132b5
adrianxx321 <lianghan321@gmail.com>
2021-08-13 02:51:15 +0800

    Cleanup unnecessary files for this branch

--- src/screens/HomeScreen.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { View, Text, Button, StyleSheet } from "react-native"
-
-const HomeScreen = ({navigation}) => {
-    return <Text></Text>
-}
-
-export default HomeScreen
--- src/screens/MoreScreen.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { View, Text, Button, StyleSheet } from "react-native"
-
-const MoreScreen = ({navigation}) => {
-    return <Text></Text>
-}
-
-export default MoreScreen
