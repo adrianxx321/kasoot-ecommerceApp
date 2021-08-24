5b5899f2accb409535e9d79323ee45dfbdcb2fb7
adrnliaw <adrnliaw@gmail.com>
2021-08-24 12:37:06 +0800

    Added Search History

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react"
-import { View, Text, Image, TouchableOpacity, StyleSheet, FlatList, TextInput, TouchableWithoutFeedback } from "react-native"
+import { View, Text, Image, TouchableOpacity, StyleSheet, FlatList, TextInput, TouchableWithoutFeedback, AsyncStorage, ScrollView } from 
+"react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
 import * as algolia from "../services/algolia"
 import ProductCard from "../components/ProductCard"
@@ -11,9 +12,69 @@ import { ScreenRatio_iPhone } from "../components/ScreenRatio-iPhone"
 const SearchScreen = ({navigation}) => {
     const [search, setSearch] = useState('')
 
+    const [searchHistoryList, setSearchHistoryList] = useState([]);
+
+    useEffect( () => {
+        readSearchHistory();
+    }, []);
+
+
+   const updateSearchHistory = async (searchHistoryList) => {
+        try {
+            await AsyncStorage.setItem('searchHistoryList', JSON.stringify(searchHistoryList));
+        } catch (error) {
+            console.log('## ERROR SAVING ITEM ##: ', error);
+        }
+    };
+
+    const readSearchHistory = async () => {
+        try {
+            const searchHistoryListString = await AsyncStorage.getItem('searchHistoryList');
+            if (searchHistoryListString !== null) {
+                setSearchHistoryList(JSON.parse(searchHistoryListString));
+            }
+        } catch (error) {
+            console.log('## ERROR READING ITEM ##: ', error);
+        }
+    };
+
+    const clearSearchHistory = async () => {
+        try{
+            await AsyncStorage.removeItem('searchHistoryList');
+            const reset = [];
+            setSearchHistoryList(reset);
+        } catch (error) {
+            console.log('## ERROR CLEARING ITEM ##: ', error);
+        }
+    }
+
+
+    const renderItem = ({ item }) => (
+        <Item phrase={item.phrase} />
+    );
+
+    const Item = ({ phrase }) => (
+        <View>
+            <TouchableWithoutFeedback
+                onPress={() => {
+                    navigation.navigate("Search Products", {passedSearchTerm: phrase});
+                }}>
+            <Text style={{
+                    color: "#000000",
+                    fontWeight: "500",
+                    fontSize: ScreenRatio_iPhone(16),
+                    padding: ScreenRatio_iPhone(7),
+                    marginHorizontal: ScreenRatio_iPhone(25),
+                    borderBottomWidth : 1,
+                    borderBottomColor: '#f58b4b',
+                }}>{phrase}</Text>
+            </TouchableWithoutFeedback>
+        </View>
+    );
+
     return (
-        <SafeAreaView
-            style={{
+        <SafeAreaView>
+            <View style={{
                 zIndex: 1,
                 marginStart: ScreenRatio_iPhone(15),
                 marginVertical: ScreenRatio_iPhone(25),
@@ -21,70 +82,110 @@ const SearchScreen = ({navigation}) => {
                 justifyContent: "space-between",
                 marginHorizontal: ScreenRatio_iPhone(15)
             }}>
-            <TouchableOpacity style={{marginEnd: ScreenRatio_iPhone(5)}}
-                onPress={() => {navigation.goBack()}}>
-                <Image
-                    source={require("../../assets/icons/back.png")}
-                    resizeMode="contain"
-                    style={{
-                        width: ScreenRatio_iPhone(30),
-                        height: ScreenRatio_iPhone(30)
-                    }}/>
-            </TouchableOpacity>
-            <View style={{
-                flexDirection: "row",
-                borderWidth: ScreenRatio_iPhone(2),
-                borderRadius: ScreenRatio_iPhone(30),
-                width: "70%",
-                borderColor: "#f58b4b",
-                justifyContent: "space-evenly",
-                bottom: ScreenRatio_iPhone(7)
-            }}>
-                <Image
-                    source={require("../../assets/icons/search.png")}
-                    resizeMode="contain"
-                    style={{
-                        width: ScreenRatio_iPhone(30),
-                        height: ScreenRatio_iPhone(30),
-                        marginStart: ScreenRatio_iPhone(7),
-                        marginVertical: ScreenRatio_iPhone(7),
-                        marginEnd: ScreenRatio_iPhone(10)
-                    }}
-                />
-                <TextInput
-                    placeholder="Search in Kasoot"
-                    keyboardType="web-search"
-                    onChangeText={(input) => setSearch(input)}
-                    style={{width: "70%", flexShrink: 1, fontSize: ScreenRatio_iPhone(18)}}
-                    value={search}
-                />
-                <TouchableWithoutFeedback onPress={() => setSearch("")}>
+                <TouchableOpacity style={{marginEnd: ScreenRatio_iPhone(5)}}
+                    onPress={() => {navigation.goBack()}}>
                     <Image
-                        source={require("../../assets/icons/close.png")}
+                        source={require("../../assets/icons/back.png")}
                         resizeMode="contain"
                         style={{
-                            marginEnd: ScreenRatio_iPhone(7),
-                            marginVertical: ScreenRatio_iPhone(10),
-                            marginStart: ScreenRatio_iPhone(10),
-                            width: ScreenRatio_iPhone(24),
-                            height: ScreenRatio_iPhone(24),
+                            width: ScreenRatio_iPhone(30),
+                            height: ScreenRatio_iPhone(30)
+                        }}/>
+                </TouchableOpacity>
+                <View style={{
+                    flexDirection: "row",
+                    borderWidth: ScreenRatio_iPhone(2),
+                    borderRadius: ScreenRatio_iPhone(30),
+                    width: "70%",
+                    borderColor: "#f58b4b",
+                    justifyContent: "space-evenly",
+                    bottom: ScreenRatio_iPhone(7)
+                }}>
+                    <Image
+                        source={require("../../assets/icons/search.png")}
+                        resizeMode="contain"
+                        style={{
+                            width: ScreenRatio_iPhone(30),
+                            height: ScreenRatio_iPhone(30),
+                            marginStart: ScreenRatio_iPhone(7),
+                            marginVertical: ScreenRatio_iPhone(7),
+                            marginEnd: ScreenRatio_iPhone(10)
                         }}
                     />
-                </TouchableWithoutFeedback>
+                    <TextInput
+                        placeholder="Search in Kasoot"
+                        keyboardType="web-search"
+                        onChangeText={(input) => setSearch(input)}
+                        style={{width: "70%", flexShrink: 1, fontSize: ScreenRatio_iPhone(18)}}
+                        value={search}
+                    />
+                    <TouchableWithoutFeedback onPress={() => setSearch("")}>
+                        <Image
+                            source={require("../../assets/icons/close.png")}
+                            resizeMode="contain"
+                            style={{
+                                marginEnd: ScreenRatio_iPhone(7),
+                                marginVertical: ScreenRatio_iPhone(10),
+                                marginStart: ScreenRatio_iPhone(10),
+                                width: ScreenRatio_iPhone(24),
+                                height: ScreenRatio_iPhone(24),
+                            }}
+                        />
+                    </TouchableWithoutFeedback>
+                </View>
+                <TouchableOpacity
+                    onPress={() => {
+                        setSearchHistoryList(searchHistoryList => [{
+                            id: searchHistoryList.length.toString(),
+                            phrase: search
+                        }, ...searchHistoryList]);
+                        updateSearchHistory(searchHistoryList);
+                        navigation.navigate("Search Products", {passedSearchTerm: search})
+                    }}>
+                    <Text style={{
+                        color: "#f58b4b",
+                        fontWeight: "500",
+                        fontSize: ScreenRatio_iPhone(16),
+                        textAlign: "center",
+                        padding: ScreenRatio_iPhone(7),
+                    }}>Search
+                    </Text>
+                </TouchableOpacity>
             </View>
-            <TouchableOpacity
+            
+            <TouchableOpacity style={{
+                flexDirection: 'row-reverse',
+                backgroundColor: '#AEA9A9',
+                alignSelf: "flex-end",
+                borderRadius: ScreenRatio_iPhone(30),
+                marginBottom: ScreenRatio_iPhone(7),
+                padding: ScreenRatio_iPhone(3),
+                paddingEnd: ScreenRatio_iPhone(10),
+                right: ScreenRatio_iPhone(10),
+                }}
                 onPress={() => {
-                    navigation.navigate("Search Products", {passedSearchTerm: search})
-                }}>
-                <Text style={{
-                    color: "#f58b4b",
-                    fontWeight: "500",
-                    fontSize: ScreenRatio_iPhone(16),
-                    textAlign: "center",
-                    padding: ScreenRatio_iPhone(7),
-                }}>Search
-                </Text>
+                    clearSearchHistory();
+                    }}>
+                <Image
+                    source={require("../../assets/icons/close.png")}
+                    resizeMode="contain"
+                    style={{
+                        marginEnd: ScreenRatio_iPhone(7),
+                        marginStart: ScreenRatio_iPhone(10),
+                        width: ScreenRatio_iPhone(24),
+                        height: ScreenRatio_iPhone(24),
+                    }}
+                />
+                <Text style={{color: "#000000"}}>Clear all history</Text>
             </TouchableOpacity>
+            <FlatList 
+                data={searchHistoryList}
+                keyExtractor={item => item.id}
+                renderItem={renderItem}
+                contentContainerStyle={{paddingBottom: ScreenRatio_iPhone(150)}}
+                style={{height: "100%"}}
+            />
+            
         </SafeAreaView>
     )
 }
