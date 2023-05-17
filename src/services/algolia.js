f400edbc09f24ccffc1ab1874855cf2815032d6d
adrianxx321 <lianghan321@gmail.com>
2023-05-17 21:26:04 +0800

    Reconstruct Firestore database & algolia search services API

--- src/services/algolia.js
+++ src/services/algolia.js
@@ -1,6 +1,6 @@
 const algoliasearch = require("algoliasearch");
-const ALGOLIA_ID = "IFD2BPPZCG";
-const ALGOLIA_SEARCH_KEY = "43b4ebd6602c07e3cb971f8042f383ad";
+const ALGOLIA_ID = "C6F1EVG98O";
+const ALGOLIA_SEARCH_KEY = "d0334b51b96b3557dbc8d20f3ed7ebd9";
 
 const searchProducts = algoliasearch(ALGOLIA_ID, ALGOLIA_SEARCH_KEY);
 const searchProductsIndex = searchProducts.initIndex('shoes');
