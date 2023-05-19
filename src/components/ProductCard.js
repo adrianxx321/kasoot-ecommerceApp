9549f401939789f2a2730986eb71cea2cfb23c7d
adrianxx321 <lianghan321@gmail.com>
2023-05-19 23:02:23 +0800

    Optimize state management and reduce unnecessary redundant queries from database

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,118 +1,131 @@
-import React, { useState, useEffect } from "react"
-import { View, Text, Image, TouchableOpacity, StyleSheet, TouchableWithoutFeedback } from "react-native"
+import React, { useEffect } from "react";
+import { View, Text, Image, TouchableOpacity, StyleSheet, TouchableWithoutFeedback } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-
-import * as FirebaseServices from "../services/firestore"
-
+import * as FirebaseServices from "../services/firestore";
 import 'intl';
 import 'intl/locale-data/jsonp/en';
-import { ScreenRatio_iPhone } from "./ScreenRatio-iPhone"
+import { ScreenRatio_iPhone } from "./ScreenRatio-iPhone";
 
 const formatter = Intl.NumberFormat('en-UK', {
-    style: "currency",
-    currency: "MYR"
-})
+  style: "currency",
+  currency: "MYR"
+});
 
-const ProductCard = ({prodID, prodImg, prodBrand, prodName, prodPrice, prodDiscount}) => {
-    const navigation = useNavigation()
-    const [wishlist, setWishlist] = useState([])
-    
-    const fetchWishlist = async (uid) => {
-        try {
-            const response = await FirebaseServices.getWishlist(uid).get()
+const ProductCard = ({ product, wishlist, isLiked, onLikeUnlike }) => {
+  const navigation = useNavigation();
 
-            if(response.exists) {
-                setWishlist(response.data().shoes)
-            }
-        } catch(err) {
-            console.error(err)
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("focus", () => {
+      // Fetch wishlist from Firebase
+      fetchWishlist(FirebaseServices.getUserID());
+    });
+
+    // Fetch wishlist from Firebase
+    fetchWishlist(FirebaseServices.getUserID());
+
+    return unsubscribe;
+  }, [navigation]);
+
+  const fetchWishlist = async (uid) => {
+    try {
+      const response = await FirebaseServices.getWishlist(uid).get();
+      if (response.exists) {
+        // Update the isLiked state if the product.id is present in the wishlist
+        const wishlist = response.data().shoes;
+        if (wishlist.includes(product.id)) {
+          onLikeUnlike(product.id, true);
         }
+      }
+    } catch (err) {
+      console.error(err);
     }
+  };
 
-    useEffect(() => {
-        const unsubscribe = navigation.addListener("focus", () => {
-            fetchWishlist(FirebaseServices.getUserID())
-        })
+  const handlePress = () => {
+    navigation.navigate("Product", { shoeID: product.id });
+  };
 
-        // Fetch wishlist from Firebase
-        fetchWishlist(FirebaseServices.getUserID())
+  const handleLikeUnlike = () => {
+    onLikeUnlike(product, !isLiked);
+  };
 
-        return unsubscribe
-    }, [navigation])
-    
-    return (
-        <TouchableWithoutFeedback
-            onPress={() => {
-                navigation.navigate("Product", {shoeID: prodID})
-            }}>
-            <View
-                style={{
-                    flexDirection: "row",
-                    backgroundColor: "#ebedf2",
-                    marginHorizontal: ScreenRatio_iPhone(15),
-                    marginBottom: ScreenRatio_iPhone(15),
-                    padding: ScreenRatio_iPhone(10),
-                    borderRadius: ScreenRatio_iPhone(40),
-                }}>
-                <Image 
-                    source={{uri: prodImg}}
-                    style={{
-                        width: ScreenRatio_iPhone(128),
-                        height: ScreenRatio_iPhone(128),
-                        backgroundColor: "#e3e5ea",
-                        borderRadius: ScreenRatio_iPhone(40),
-                    }}
-                />
-                <View style={{
-                    flexShrink: 1,
-                    marginLeft: ScreenRatio_iPhone(16),
-                    marginTop: ScreenRatio_iPhone(12),
-                    width: "100%"
-                }}>
-                    <Text style={[styles.cardText, {fontWeight: "bold", lineHeight: ScreenRatio_iPhone(22)}]}>
-                        {prodBrand} {prodName}
-                    </Text>
-                    <Text style={[styles.cardText, {textDecorationLine: (prodDiscount) ? "line-through" : "none"}]}>
-                        {formatter.format(prodPrice)}
-                    </Text>
-                    <Text style={[styles.cardText, {display: (prodDiscount) ? "flex" : "none", fontWeight: "bold", color: "#de651d"}]}>
-                        {formatter.format(prodPrice * (1 - prodDiscount / 100))}
-                    </Text>
-                </View>
-                <View style={{marginEnd: ScreenRatio_iPhone(16), justifyContent: "center"}}>
-                    <TouchableOpacity
-                        onPress={() => {
-                            if (wishlist.includes(prodID)) {
-                                // remove
-                                setWishlist(wishlist.filter((e)=>(e !== prodID)))
-                                FirebaseServices.removeShoeFromWishlist(FirebaseServices.getUserID(), prodID)
-                            }
-                            else {
-                                // add
-                                setWishlist([...wishlist, prodID])
-                                FirebaseServices.addToShoeWishlist(FirebaseServices.getUserID(), prodID)
-                            }
-                        }}>
-                        <Image 
-                            source={(wishlist.includes(prodID) && wishlist.length > 0) ? require("../../assets/icons/wishlist-selected.png") : require("../../assets/icons/wishlist.png")}
-                            style={{
-                                width: ScreenRatio_iPhone(24),
-                                height: ScreenRatio_iPhone(24),
-                                tintColor: (wishlist.includes(prodID)) ? "#e30022" : "#000000"
-                            }}
-                        />
-                    </TouchableOpacity>
-                </View>
-            </View>
-        </TouchableWithoutFeedback>
-    )
-}
+  return (
+    <TouchableWithoutFeedback onPress={handlePress}>
+      <View
+        style={{
+          flexDirection: "row",
+          backgroundColor: "#ebedf2",
+          marginHorizontal: ScreenRatio_iPhone(15),
+          marginBottom: ScreenRatio_iPhone(15),
+          padding: ScreenRatio_iPhone(10),
+          borderRadius: ScreenRatio_iPhone(40),
+        }}
+      >
+        <Image
+          source={{ uri: product.prodImg[0] }}
+          style={{
+            width: ScreenRatio_iPhone(128),
+            height: ScreenRatio_iPhone(128),
+            backgroundColor: "#e3e5ea",
+            borderRadius: ScreenRatio_iPhone(40),
+          }}
+        />
+        <View
+          style={{
+            flexShrink: 1,
+            marginLeft: ScreenRatio_iPhone(16),
+            marginTop: ScreenRatio_iPhone(12),
+            width: "100%",
+          }}
+        >
+          <Text
+            style={[styles.cardText, { fontWeight: "bold", lineHeight: ScreenRatio_iPhone(22) }]}
+          >
+            {product.prodBrand} {product.prodName}
+          </Text>
+          <Text
+            style={[
+              styles.cardText,
+              { textDecorationLine: product.prodDiscount ? "line-through" : "none" },
+            ]}
+          >
+            {formatter.format(product.prodPrice)}
+          </Text>
+          <Text
+            style={[
+              styles.cardText,
+              { display: product.prodDiscount ? "flex" : "none", fontWeight: "bold", color: "#de651d" },
+            ]}
+          >
+            {formatter.format(product.prodPrice * (1 - product.prodDiscount / 100))}
+          </Text>
+        </View>
+        <View style={{ marginEnd: ScreenRatio_iPhone(16), justifyContent: "center" }}>
+          <TouchableOpacity onPress={handleLikeUnlike}>
+            <Image
+              source={
+                isLiked && wishlist.length > 0
+                  ? require("../../assets/icons/wishlist-selected.png")
+                  : require("../../assets/icons/wishlist.png")
+              }
+              style={{
+                width: ScreenRatio_iPhone(24),
+                height: ScreenRatio_iPhone(24),
+                tintColor: isLiked ? "#e30022" : "#000000",
+              }}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
 
 const styles = StyleSheet.create({
-    cardText: {
-        fontSize: ScreenRatio_iPhone(18),
-        lineHeight: ScreenRatio_iPhone(44),
-    }
-})
+  cardText: {
+    fontSize: ScreenRatio_iPhone(18),
+    lineHeight: ScreenRatio_iPhone(44),
+  },
+});
 
-export default ProductCard
+export default ProductCard;
--- src/screens/Wishlist.js
+++ src/screens/Wishlist.js
@@ -1,141 +1,155 @@
-import React, { useState, useEffect } from "react"
-import { View, Text, Image, TouchableOpacity, StyleSheet, FlatList } from "react-native"
-import { SafeAreaView } from "react-native-safe-area-context"
-import ProductCard from "../components/ProductCard"
+import React, { useState, useEffect } from "react";
+import { View, Text, Image, TouchableOpacity, StyleSheet, FlatList } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import ProductCard from "../components/ProductCard";
+import * as FirebaseServices from "../services/firestore";
+import 'intl';
+import 'intl/locale-data/jsonp/en';
+import { ScreenRatio_iPhone } from "../components/ScreenRatio-iPhone";
 
-import * as FirebaseServices from "../services/firestore"
+const Wishlist = ({ navigation }) => {
+const [wishlist, setWishlist] = useState([]);
+const [products, setProducts] = useState([]);
 
-import 'intl'
-import 'intl/locale-data/jsonp/en'
-import { ScreenRatio_iPhone } from "../components/ScreenRatio-iPhone"
+const fetchWishlistAndProds = async (uid) => {
+    try {
+      const wishlistResponse = await FirebaseServices.getWishlist(uid).get();
+      
+      if (wishlistResponse.exists) {
+          const wishlistData = wishlistResponse.data();
+          setWishlist(wishlistData.shoes);
+          
+          if (wishlistData.shoes.length > 0) {
+            const productResponse = await FirebaseServices.getShoesByID(wishlistData.shoes);
+            const productsData = [];
+            
+            productResponse.docs.forEach((shoe) => {
+                const productData = shoe.data();
+                productData.id = shoe.id;
+                productsData.push(productData);
+            });
+            setProducts(productsData);
+          } else {
+            setProducts([]);
+          }
+      } else {
+          setWishlist([]);
+          setProducts([]);
+      }
+    } catch(err) {
+      console.error(err);
+    }
+};
 
-const Wishlist = ({navigation}) => {
-    const [wishlist, setWishList] = useState([])
-    const [products, setProducts] = useState([])
+useEffect(() => {
+    const uid = FirebaseServices.getUserID();
+    fetchWishlistAndProds(uid);
 
-    const fetchWishlist = async (uid) => {
-        try {
-            const response = await FirebaseServices.getWishlist(uid).get()
+    const unsubscribe = navigation.addListener("focus", () => {
+      fetchWishlistAndProds(uid);
+    });
+
+    return unsubscribe;
+}, [navigation]);
+
+const handleLikeUnlike = async (product, isLiked) => {
+    const uid = FirebaseServices.getUserID();
+
+    try {
+      if (isLiked) {
+        await FirebaseServices.addToShoeWishlist(uid, product.id);
+
+        setWishlist([...wishlist, product.id]);
+        setProducts([...products, products.push(product)])
+      } else {
+        await FirebaseServices.removeShoeFromWishlist(uid, product.id);
         
-            if(response.exists) {
-                setWishList(response.data().shoes)
-            }
-        }
-        catch(err) {
-            console.error(err)
-        }
+        setWishlist(wishlist.filter((e) => e !== product.id));
+        setProducts(products.filter((e) => e.id !== product.id))
+      }
+    } catch (err) {
+      console.error(err);
     }
+};
 
-    const fetchProds = async (ids) => {
-        try {
-            var result = []
-            const response = await FirebaseServices.getShoesByID(ids)
-
-            if(!response.empty) {
-                response.docs.forEach((shoe) => {
-                    result.push(shoe.data())
-                    result[result.length - 1].id = shoe.id
-                })
-                setProducts(result)
-            }
-        }
-        catch(err) {
-            console.error(err)
-        }
-    }
-
-    useEffect(() => {
-        const unsubscribe = navigation.addListener("focus", () => {
-            fetchWishlist(FirebaseServices.getUserID())
-            fetchProds(wishlist)
-        })
-
-        fetchWishlist(FirebaseServices.getUserID())
-        fetchProds(wishlist)
-
-        return unsubscribe
-    }, [navigation, wishlist, products])
-
-    const renderHeader = () => {
-        return (
-            <SafeAreaView
-                style={{
-                    zIndex: 3,
-                    marginHorizontal: ScreenRatio_iPhone(15),
-                    marginVertical: ScreenRatio_iPhone(15),
-                    flexDirection: "row",
-                    justifyContent: "space-between",
-                    position: "absolute",
-                    left: 0,
-                    right: 0
-                }}>
-                <TouchableOpacity
-                    onPress={() => navigation.goBack()}>
-                    <Image
-                        source={require("../../assets/icons/back.png")}
-                        resizeMode="contain"
-                        style={{
-                            width: ScreenRatio_iPhone(30),
-                            height: ScreenRatio_iPhone(30),
-                        }}
-                    />
-                </TouchableOpacity>
-            </SafeAreaView>
-        )
-    }
-
-    const renderTitle = () => {
-        return (
-            <SafeAreaView style={styles.pageHeader}>
-                <Text style={{
-                    fontWeight: "bold",
-                    fontSize: ScreenRatio_iPhone(36)
-                }}>My Wishlist</Text>
-                <Text style={{
-                    color: "#a3a3a3",
-                    fontSize: ScreenRatio_iPhone(24)
-                }}>{wishlist.length} items</Text>
-            </SafeAreaView>
-        )
-    }
-
-    const renderWishlistItem = ({item}) => (
-        <ProductCard
-            prodID={item.id}
-            prodImg={item.prodImg[0]}
-            prodBrand={item.prodBrand}
-            prodName={item.prodName}
-            prodPrice={item.prodPrice}
-            prodDiscount={item.prodDiscount}
-            key={item.id}
-        />
-    )
-
+const renderHeader = () => {
     return (
-        <View>
-            {renderHeader()}
-            <View style={{marginVertical: ScreenRatio_iPhone(64)}}>
-                {renderTitle()}
-                <FlatList
-                    data={products}
-                    renderItem={renderWishlistItem}
-                    keyExtractor={item => item.objectID}
-                    style={{marginTop: ScreenRatio_iPhone(20), height: "100%"}}
-                />
-            </View>
-        </View>
+      <SafeAreaView
+          style={{
+              zIndex: 3,
+              marginHorizontal: ScreenRatio_iPhone(15),
+              marginVertical: ScreenRatio_iPhone(15),
+              flexDirection: "row",
+              justifyContent: "space-between",
+              position: "absolute",
+              left: 0,
+              right: 0
+          }}>
+          <TouchableOpacity
+              onPress={() => navigation.goBack()}>
+              <Image
+                  source={require("../../assets/icons/back.png")}
+                  resizeMode="contain"
+                  style={{
+                      width: ScreenRatio_iPhone(30),
+                      height: ScreenRatio_iPhone(30),
+                  }}
+              />
+          </TouchableOpacity>
+      </SafeAreaView>
     )
 }
 
+const renderTitle = () => {
+    return (
+      <SafeAreaView style={styles.pageHeader}>
+          <Text style={{
+              fontWeight: "bold",
+              fontSize: ScreenRatio_iPhone(36)
+          }}>My Wishlist</Text>
+          <Text style={{
+              color: "#a3a3a3",
+              fontSize: ScreenRatio_iPhone(24)
+          }}>{products.length} items</Text>
+      </SafeAreaView>
+    )
+}
+
+const renderWishlistItem = ({item}) => (
+    <ProductCard
+      product={item}
+      wishlist={wishlist}
+      isLiked={wishlist.includes(item.id)}
+      onLikeUnlike={handleLikeUnlike}
+      key={item.objectID}
+    />
+);
+
+return (
+    <View>
+      {renderHeader()}
+      <View style={{ marginVertical: ScreenRatio_iPhone(64) }}>
+        {renderTitle()}
+        <FlatList
+        data={products}
+        renderItem={renderWishlistItem}
+        keyExtractor={(item) => item.objectID}
+        style={{ marginTop: ScreenRatio_iPhone(20), height: "100%" }}
+        />
+      </View>
+    </View>
+);
+};
+
 const styles = StyleSheet.create({
     pageHeader: {
-        marginHorizontal: ScreenRatio_iPhone(25),
-        marginTop: ScreenRatio_iPhone(25),
-        marginBottom: ScreenRatio_iPhone(10),
-        flexDirection: "row",
-        alignItems: "baseline",
-        justifyContent: "space-between",
+      marginHorizontal: ScreenRatio_iPhone(25),
+      marginTop: ScreenRatio_iPhone(25),
+      marginBottom: ScreenRatio_iPhone(10),
+      flexDirection: "row",
+      alignItems: "baseline",
+      justifyContent: "space-between",
     }
 })
 
-export default Wishlist
+export default Wishlist;
